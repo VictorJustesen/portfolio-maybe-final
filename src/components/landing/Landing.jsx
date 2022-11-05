21604117f83dc066197a0917d0ecf4f191ca5425
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2022-11-05 22:21:26 +0100

    nav

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -9,7 +9,7 @@ export default function Landing(){return(
     <h1 className='laheading' >Victor Justesen</h1>
     <h2 className='lasubheading'> Jeg har brug for et arbejde</h2>
     <div className='latext'>
-        <p className='text'> jeg er en ung studerende,
+        <p className='text'> Jeg er en ung studerende,
         og leder efter et sted hvor jeg kan bruge, og viderudvikle mine it evner.
         Lige nu er jeg rimelig hooked på at lave hjemmesider og andet front-end arbejde, men det kan hurtigt ændre sig
         </p>
