d70908745b7775eba2f1b3bdfe0ea2507279598c
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2022-11-07 17:20:42 +0100

    okok

--- src/components/ability/Ability.js
+++ src/components/ability/Ability.js
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 
 export default function Ability(props){
 
-  const { ref:first, inView:infirst } = useInView({"triggerOnce": true, "delay": 750, "threshold":1 });
+  const { ref:first, inView:infirst } = useInView({"triggerOnce": true, "delay": 500, "threshold":1 });
 
     return(
 
