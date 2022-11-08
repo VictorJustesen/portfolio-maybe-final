4ca341962d7f47f392a33eb1cf1dc15f8a1a7bfe
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2022-11-08 09:02:36 +0100

    animation fiddling

--- src/components/ability/Abilities.jsx
+++ src/components/ability/Abilities.jsx
@@ -6,19 +6,19 @@ import { useInView } from 'react-intersection-observer';
 
 export default function Abilities(){
 
-    const { ref:first, inView:infirst } = useInView({"triggerOnce": true, "delay": 200, "threshold":0.19 });
-    const { ref:second, inView:insecond } = useInView({"triggerOnce": true, "delay":  300,"threshold":0.05 });
+    const { ref:first, inView:infirst } = useInView({"triggerOnce": true, "delay": 100, "threshold":0.01 });
+    const { ref:second, inView:insecond } = useInView({"triggerOnce": true, "delay":  400,"threshold":0.025 });
 
     return(
     <section  className='container abilities__container'>
 
         <h1 id="abilites"  className={`${infirst ? "fadeIn heading" : "hidden"}`} >Kompetencer</h1>
         <h2  className={`${insecond ? "fadeIn subheading" : "hidden"}`}>Noget af det jeg kan lide at rode med</h2>
-        <div ref={second} className='portfolio'>
+        <div ref={first} className='portfolio'>
       
             
                 
-        <div ref={first} className="skills"> 
+        <div ref={second} className="skills"> 
             
             <Skill  text="ReactJS" classtext="devicon-react-original colored" level="mellem"/>
             <Skill ref={second} text="Python" classtext="devicon-python-plain colored" level="mellem"/>
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,32 +5,33 @@ import { useInView } from 'react-intersection-observer';
 
 export default function About(){
     
-    const { ref:first, inView:infirst } = useInView({"triggerOnce": true, "delay": 350 ,"threshold":1});
-    const { ref:second, inView:insecond } = useInView({"triggerOnce": true, "delay": 1200 ,});
-    const { ref:third, inView:inthird } = useInView({"triggerOnce": true, "delay": 500,"threshold":0.5 });
-    const { ref:fourth, inView:infourth } = useInView({"triggerOnce": true, "delay": 750,"threshold":0.5 });
+    const { ref:first, inView:infirst } = useInView({"triggerOnce": true,"delay": 400, "threshold":0.05});
+    const { ref:second, inView:insecond } = useInView({"triggerOnce": true, "delay": 1000, "rootMargin":"-20% 0px"});
+    const { ref:third, inView:inthird } = useInView({"triggerOnce": true, "delay": 950,"threshold":1 });
+    const { ref:fourth, inView:infourth } = useInView({"triggerOnce": true, "delay": 1300,"threshold":0.5 });
     
     return(
         
 <section  className='container about__container'>
-<h1 ref={first} className={`${infirst ? "fadeIn heading" : "hidden"}`}>Om mig</h1>
+<h1  className={`${infirst ? "fadeIn heading" : "hidden"}`}>Om mig</h1>
 
-    <div className='aimgb'>
+    <div ref={first} className='aimgb'>
 
     <img ref={second} className={`${insecond ? "fadeIn aimg" : "hidden"}`} src={ME} alt="me"/>
     
    <div className='abouttext'>
-    <h5 ref={third} className={`${inthird ? "fadeIn text" : "hidden"}`}>
+    <h5  className={`${inthird ? "fadeIn text" : "hidden"}`}>
 Jeg er en ung mand på 21 som nyder liver på DTU hvor jeg læser softwareteknologi.
  Jeg har dog et stort ønske om at kunne bruge mine evner i et mere proffesionelt miljø,
 og jeg søger derfor et studiejob.
 
     </h5>
-    <div className='space'></div>
-    <h5 ref={fourth} className={`${infourth ? "fadeIn text" : "hidden"}`}>
+    <div ref={third} className='space'></div>
+    <h5  className={`${infourth ? "fadeIn text" : "hidden"}`}>
     Jeg kan godt lide at smarte løsninger som er scalerbare, justerbare og giver mening. 
     Jeg kan lidt af hvert, med ekstra kompetence inden for web development. 
     Hvis du mangler en som lærer hurtigt så tøv ikke med at skrive til mig!  
+    <div ref={fourth}></div>
     </h5>
     </div>
     </div>
