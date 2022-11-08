3b341390e93f953948e8013fdc89577a54768843
VictorJustesen <102155703+VictorJustesen@users.noreply.github.com>
2022-11-08 20:32:56 +0100

    contact

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ function App() {
 	return (
 		<div className='App'>
 			<Sideheaderleft/>
-			<Sideheaderright/>
+			
 			<Landing/>
 			<About/>
 			<Abilities/>
 			<Nav/>
 			<Contact/>
 			<Footer/>
+			<Sideheaderright/>
 		</div>
 	);
 }
